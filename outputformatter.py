a46f2aef204c1cf13e337d51d90c200989e00378
zv_nik <105583773+ZverevNikita@users.noreply.github.com>
2022-11-10 15:07:38 +0300

    Delete outputformatter.py

--- outputformatter.py
+++
@@ -1,9 +0,0 @@
-def formoutput(pack: dict) -> str:
-    msg = "Вот, что нам удалось найти:\n\n\n"
-    counter = 1
-    for key in pack:
-        msg += str(counter) + ". " + str(pack[key]) + "\n\n"
-        counter += 1
-    msg += "\nВыберите номер снизу, чтобы добавить напоминание за день до события:"
-
-    return msg
